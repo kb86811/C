dd449df2676fe3c30fb4b846f6badd2d79c9ef58
Kedir Birhan <kedirbirhan@yahoo.com>
2019-02-03 21:29:44 -0500

    commit

--- project2/project2_kbirhan_202.c
+++ project2/project2_kbirhan_202.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include "supporterFunction.h"
 #define MAX_CHAIN_BYTES  100
-//uhdudh
+//uhdudh   // "${workspaceFolder}/project2/project2_kbirhan_202"
+///Users/kedir/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/usr/bin
+
 typedef struct _Chain
 {
 	size_t  Seqlen;  // Number of letters in sequence
